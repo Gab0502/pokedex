7750646f4baeec5f58b30e2bc02f7ed1306235b2
GAbriel <gabmt2210@gmail.com>
2023-05-15 11:51:27 -0300

    fonte

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let horas = new Date();
-let hora = (horas.getHours()-2);
-let cont=0
+let hora = (horas.getHours() - 2);
+let cont = 0
 console.log(cont)
 document.write(hora)
-if (hora<10){
+if (hora < 10) {
     document.body.style.backgroundImage = "url('imgs/bg1.jpg')";
 }
-else if (hora<20){
+else if (hora < 20) {
     document.body.style.backgroundImage = "url('imgs/bg2.jpg')";
 }
-else{
+else {
     document.body.style.backgroundImage = "url('imgs/bg3.jpg'";
 }
 const tela = document.querySelector('#mudar');
 const button = document.querySelector('#ligar');
 
-button.addEventListener('click', ()=>{
+button.addEventListener('click', () => {
     cont++
-    switch(cont){
+    switch (cont) {
         case 1:
-            tela.src='imgs/ligado.png'
-           break
-        
-        case 2:
-            tela.src='imgs/desligado.png'
+            tela.src = 'imgs/ligado.png'
             break
-        }
-    if (cont==2){
+
+        case 2:
+            tela.src = 'imgs/desligado.png'
+            break
+    }
+    if (cont == 2) {
         cont = 0
     }
 })
